6f55ff8ba555c6e7d597f19f9e890b7c22f31b9e
Lewin Kelly <heli@odex.cf>
2026-05-12 12:10:12 +0100

    ciksa pruce fi lo genjva klesi

--- genturfa'i/main.go
+++ genturfa'i/main.go
@@ -25,4 +25,32 @@ func main() {
 	}
 
 	fmt.Println(gerna)
+
+	for cmene, genjva := range gerna {
+		mekso := genjva.Expr
+
+		fmt.Println(cmene, ":", mekso)
+		switch mekso.(type) {
+		case ebnf.Alternative:
+			fmt.Println("ebnf.Alternative")
+		case ebnf.Sequence:
+			fmt.Println("ebnf.Sequence")
+		case *ebnf.Name:
+			fmt.Println("*ebnf.Name")
+		case *ebnf.Token:
+			fmt.Println("*ebnf.Token")
+		case *ebnf.Range:
+			fmt.Println("*ebnf.Range")
+		case *ebnf.Group:
+			fmt.Println("*ebnf.Group")
+		case *ebnf.Option:
+			fmt.Println("*ebnf.Option")
+		case *ebnf.Repetition:
+			fmt.Println("*ebnf.Repetition")
+		case *ebnf.Production:
+			fmt.Println("*ebnf.Production")
+		case *ebnf.Bad:
+			fmt.Println("*ebnf.Bad")
+		}
+	}
 }
